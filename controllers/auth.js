4c30e4fb89e8eaa8dd53830d98f4e90ebc22a2c4
sahartak <sahartak@yandex.com>
2019-02-03 18:37:05 +0400

    register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,33 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 module.exports = {
 
     login(req, resp) {
-        resp.status(200).json({
-            login: {
-                email: req.body.email,
-                password: req.body.password
-            }
-        });
+        
     },
 
-    register(req, resp) {
-        resp.status(200).json({
-            message: 'register from controller'
-        });
+    async register(req, resp) {
+        let requestBody = req.body;
+        let user = await User.findOne({email: requestBody.email});
+        if(user) {
+            resp.status(409).json({
+                message: 'User already exists!'
+            });
+        } else {
+            let salt = bcrypt.genSaltSync(10);
+            let password = requestBody.password;
+            user = new User({
+                email: requestBody.email,
+                password: bcrypt.hashSync(password, salt)
+            });
+
+            try {
+                await user.save();
+                resp.status(201).json(user);
+            } catch (error) {
+                // handle error
+            }
+        }
     }
 }
